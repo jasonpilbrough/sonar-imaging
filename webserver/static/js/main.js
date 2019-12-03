295177c5d9c7bf7623bfa2c1d4b2509afdde01c4
Jason <jasonpilbrough@gmail.com>
2019-12-03 18:18:13 +0200

    change how plots are loaded in javascript

--- webserver/static/js/main.js
+++ webserver/static/js/main.js
@@ -1,20 +1,32 @@
 var globaltime = new Date();
 
 function loadDoc() {
+
+		var time =  new Date();
+		console.log(time-globaltime)
+		globaltime = time
+		
+		img = document.getElementById("mainplot");
+		img.src="plot.png?rand_number=" + Math.random(); // random number forces browser to reload image
+		
+		/*
 		var time =  new Date();
 		console.log(time-globaltime)
 		globaltime = time
   		var xhttp = new XMLHttpRequest();
   		xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
+    		var blob = this.response;
+    		//console.log(blob)
     		document.getElementById("mainplot").src = "plot.png";
-      		//document.getElementById("demo").innerHTML = this.responseText
+      		//document.getElementById("mainplot") = blob
       		//var d2 = new Date();
       		//console.log(d2-d1)
     	}
   		};
 		xhttp.open("GET", "plot.png", true);
   		xhttp.send();
+  		*/
   		
   		
   		
